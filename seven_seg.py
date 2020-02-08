72ca6225c485bce59d85c5d69d881a9fa746a113
Jacob Hillock <jacobhillock@protonmail.com>
2020-02-08 11:53:28 -0500

    Update seven_seg.py

--- seven_seg.py
+++ seven_seg.py
@@ -1,11 +1,11 @@
-def seven_seg(number, x: int, y: int, scale: float) -> list:
+def seven_seg(number:str, x: int, y: int, scale: float) -> list:
     segments = [
        #(x, y, horiz, vert),
         (x + 2*scale, y, 10*scale, 2*scale),                # 0 top hoiz
         (x + 2*scale, y + 12*scale, 10*scale, 2*scale),     # 1 mid hoiz
-        (x + 2*scale, y + 12*scale, 10*scale, 2*scale),     # 2 bot hoiz
+        (x + 2*scale, y + 24*scale, 10*scale, 2*scale),     # 2 bot hoiz
         (x, y + 2*scale, 2*scale, 10*scale),                # 3 top left
-        (x + 12*scale + 2*scale, y, 2*scale, 10*scale),     # 4 top right
+        (x + 12*scale, y + 2*scale, 2*scale, 10*scale),     # 4 top right
         (x, y + 14*scale, 2*scale, 10*scale),               # 5 bot left
         (x + 12*scale, y + 14*scale, 2*scale, 10*scale)     # 6 bot right
     ]
@@ -33,10 +33,12 @@ def seven_seg(number, x: int, y: int, scale: float) -> list:
         nums = [0,1,2,3,4,5,6]
     elif number == "9":
         nums = [0,1,3,4,6]
+    elif number == "-":
+        nums = [1]
     
     for i in nums:
         retList.append(segments[i])
     
     return retList
 
-print(seven_seg("4",0,0,1))
+# print(seven_seg("4",0,0,1))
